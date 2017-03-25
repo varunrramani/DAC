7695d7dfff11f09ae693244ec0bc9ec7f8e68c0c
Varun Ramani <varun.hellraiser@gmail.com>
2017-03-25 18:09:49 +0530

    Changed Destructor to call a recursive function to delete Nodes Added Comments

--- DS/LinkedList2/LinkedList2/Source.cpp
+++ DS/LinkedList2/LinkedList2/Source.cpp
@@ -1,4 +1,4 @@
-/************************************************************************
+/********************************************************************************************
 Program: SingleLinkedListOOP.cpp
 Author: Varun Ramani (VR)
 Description: Linked List accepts Values till the user enters 0.
@@ -7,12 +7,14 @@ After the user enters 0 the linked list is displayed
 Revision History:
 25-03-2017 (VR): Project Created
 25-03-2017 (VR): Added Destructor, Printing Nodes in Reverse
-*************************************************************************
+25-03-2017 (VR): Changed destructor to call a recursive function to call destructor of Node
+********************************************************************************************
 */
 //Include Header Files
 #include <iostream>
 using namespace std;
 
+//Class definition for Linked List Node
 class Node {
 public:
 	int value;
@@ -27,14 +29,34 @@ public:
 	}
 };
 
+
+//Class Definition for Single Linked List holding Head and Tail of the Linked List
 class SingleLinkedList {
 	Node *head, *tail;
+	/*
+	Function: printRev
+	Return Type: void
+	Parameter: Node whose value to be printed
+	Description: Recursive Function to print the values stored in Linked List in the Reverse Order
+	*/
 	void printRev(Node *node) {
 		if (!node) {
 			return;
 		}
 		printRev(node->next);
 		cout << node->value << endl;
+	}
+
+	/*
+	Function: deleteNodes
+	Parameters: Node pointer pointing to head
+	*/
+	void deleteNodes(Node *node) {
+		if (!node) {
+			return;
+		}
+		deleteNodes(node->next);
+		delete node;
 	}
 public:
 	SingleLinkedList() {
@@ -43,15 +65,22 @@ public:
 	}
 
 	~SingleLinkedList() {
-		Node *temp;
+		/*Node *temp;
 		while (head) {
 			temp = head;
 			head = head->next;
 			delete temp;
 			//temp = head;
-		}
+		}*/
+		cout << "Inside SingleLinkedList Destructor" << endl;
+		deleteNodes(head);
 	}
 
+	/*
+	Function: addNode
+	Return type: void
+	Parameter: Value to be stored in Linked List Node
+	*/
 	void addNode(int value) {
 		Node *newNode = new Node(value);
 		if (NULL == head) {
@@ -64,12 +93,24 @@ public:
 		}
 	}
 
+	/*
+	Function: printAll
+	Return Type: void
+	Parameters: none
+	Description: Traverse the Linked List and Print the values stored in the order entered
+	*/
 	void printAll() {
 		for (Node *current = head; current; current = current->next) {
 			cout << current->value << endl;
 		}
 	}
 
+	/*
+	Function: printAllRev
+	Return Type: void
+	Parameter: None
+	Description: Call Recursive Function to print the values stored in Linked List in the Reverse Order
+	*/
 	void printAllRev() {
 		printRev(head);
 	}
