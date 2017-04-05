6eea9d90fdbf7876de16631ee7198fedc9f75145
Varun Ramani <varun.hellraiser@gmail.com>
2017-04-05 18:52:44 +0530

    Question 6 done

---
+++ JAVA/Assignment1/Question6.java
@@ -0,0 +1,8 @@
+public class Question6 {
+	int var1;
+
+	public static void main(String[] args) {
+		Question6 sample = new Question6();
+		System.out.println("Value of var1: " + sample.var1);
+	}
+}
