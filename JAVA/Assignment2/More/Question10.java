3457e458324e6e5ffd562e286e9821f6b505a35e
Varun Ramani <varun.hellraiser@gmail.com>
2017-04-10 18:18:31 +0530

    More Assignment Question 10 done

---
+++ JAVA/Assignment2/More/Question10.java
@@ -0,0 +1,42 @@
+/*
+10)	Define an interface "Vechicle" with "start()" function . 
+Now derive  classes like "TwoWheeler", "ThreeWheeler","FourWheeler" etc. from "Vehicle" and override "start()" function. 
+Define a class "VDemo" in which  write  main()  function. 
+In the main function create a reference to Vehicle  class referring to any of the sub class. Using this reference, call "start" method.
+*/
+interface Vehicle {
+	void start();
+}
+
+class TwoWheeler implements Vehicle {
+	@Override
+	public void start() {
+		System.out.println("TwoWheeler start");
+	}
+}
+
+class ThreeWheeler implements Vehicle {
+	@Override
+	public void start() {
+		System.out.println("ThreeWheeler start");
+	}
+}
+
+class FourWheeler implements Vehicle {
+	@Override
+	public void start() {
+		System.out.println("FourWheeler start");
+	}
+}
+
+public class Question10 {
+	public static void main(String[] args) {
+		Vehicle twoWheeler = new TwoWheeler();
+		Vehicle threeWheeler = new ThreeWheeler();
+		Vehicle fourWheeler = new FourWheeler();
+
+		twoWheeler.start();
+		threeWheeler.start();
+		fourWheeler.start();
+	}
+}
