342c5b849ab1402296af3d7d105f6754a550acb4
Varun Ramani <varun.hellraiser@gmail.com>
2017-04-12 20:46:42 +0530

    Assignment 4 (Multi-Threading) Question 2 done

--- JAVA/Assignment4/Question1/src/Question1.java
+++ JAVA/Assignment4/Question1/src/Question1.java
@@ -6,12 +6,14 @@
 /*
  * 1) create a multi-threaded application by using "extends Thread "
  * method.create 2 threads. they should display characters from A to J.
+ * 
+ * 2) above program using "implements Runnable" method.
  */
 
 class ThreadDemo extends Thread {
 	@Override
 	public void run() {
-		//super.run();
+		// super.run();
 		for (char ch = 'A'; ch <= 'J'; ch++) {
 			System.out.println(ch);
 			try {
@@ -22,6 +24,23 @@ class ThreadDemo extends Thread {
 		}
 	}
 }
+
+class ThreadDemoRunnable implements Runnable {
+
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+		for (char ch = 'A'; ch <= 'J'; ch++) {
+			System.out.println("Runnable " + ch);
+			try {
+				Thread.sleep(500);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+}
+
 public class Question1 {
 
 	/**
@@ -31,6 +50,10 @@ public class Question1 {
 		// TODO Auto-generated method stub
 		ThreadDemo threadDemo = new ThreadDemo();
 		threadDemo.start();
+
+		ThreadDemoRunnable threadDemoRunnable = new ThreadDemoRunnable();
+		Thread thread = new Thread(threadDemoRunnable);
+		thread.start();
 	}
 
 }
