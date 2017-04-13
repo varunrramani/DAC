1b7c894c02c1cf939b47621c43b22bb8971ffc87
Varun Ramani <varun.hellraiser@gmail.com>
2017-04-13 19:08:47 +0530

    Assignment 4 (Multi-Threading) Question 3 Done

---
+++ JAVA/Assignment4/Question3/src/Question3.java
@@ -0,0 +1,38 @@
+/**
+ * @author Varun Ramani
+ *
+ */
+/*
+ * 3) create 3 threads in such a way that while one thread is executing, 2
+ * threads cannot interfere. they should display output "Exec  0" to "Exec 5".
+ */
+
+class SynchronisedDemo implements Runnable {
+
+	@Override
+	synchronized public void run() {
+		// TODO Auto-generated method stub
+		for (int i = 0; i < 6; i++) {
+			System.out.println("Exec " + i);
+		}
+	}
+
+}
+
+public class Question3 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		SynchronisedDemo synchronisedDemo = new SynchronisedDemo();
+		Thread t1 = new Thread(synchronisedDemo);
+		Thread t2 = new Thread(synchronisedDemo);
+		Thread t3 = new Thread(synchronisedDemo);
+		t1.start();
+		t2.start();
+		t3.start();
+	}
+
+}
