647511b6a38ad851cf8b8e253b4fd85af32de247
Varun Ramani <varun.hellraiser@gmail.com>
2017-06-23 14:15:39 +0530

    Added Metadata for Department

---
+++ DOTNET/MVCModelFirst/MVCModelFirst/Models/DepartmentMetadata.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace MVCModelFirst.Models
+{
+    [MetadataType(typeof(DepartmentMetadata))]
+    public partial class Department
+    { }
+    public class DepartmentMetadata
+    {
+        [Display(Name = "Department Name")]
+        public String Name { get; set; }
+    }
+}
