9a165619c893e4baef3cbad12ab49ed471ee1d05
Varun Ramani <varun.hellraiser@gmail.com>
2017-06-26 14:32:41 +0530

    [LABTEST] MVC Display from DB done

---
+++ DOTNET/LabTestMVCDisplay/LabTestMVCDisplay/Controllers/HomeController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using LabTestMVCDisplay.Models;
+
+namespace LabTestMVCDisplay.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            BusinessLogic businessLogic = new BusinessLogic();
+            List<Book> list = businessLogic.GetBooks();
+            return View(list);
+        }
+    }
+}
---
+++ DOTNET/LabTestMVCDisplay/LabTestMVCDisplay/Models/Book.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+
+namespace LabTestMVCDisplay.Models
+{
+    public class Book
+    {
+        public Int32 Id { get; set; }
+        public String Name { get; set; }
+        public String Author { get; set; }
+        public Double Price { get; set; }
+        public Double ISBN { get; set; }
+        public int Quantity { get; set; }
+    }
+
+    public class BusinessLogic
+    {
+        SqlConnection GetConnection()
+        {
+            String connectionString = ConfigurationManager.ConnectionStrings["BooksDB"].ConnectionString;
+            SqlConnection connection = new SqlConnection(connectionString);
+            return connection;
+        }
+
+        public List<Book> GetBooks()
+        {
+            SqlConnection conn = GetConnection();
+            SqlCommand cmd = new SqlCommand("SELECT * FROM BOOKS", conn);
+            conn.Open();
+            SqlDataReader reader = cmd.ExecuteReader();
+            List<Book> bookList = new List<Book>();
+
+            while (reader.Read())
+            {
+                Book b = new Book();
+                b.Id = Convert.ToInt32(reader["Id"]);
+                b.Name = (reader["Name"]).ToString();
+                b.Author = (reader["Author"]).ToString();
+                b.Price = Convert.ToDouble(reader["Price"]);
+                b.ISBN = Convert.ToDouble(reader["ISBN"]);
+                b.Quantity = Convert.ToInt32(reader["Quantity"]);
+                bookList.Add(b);
+            }
+            conn.Close();
+            return bookList;
+        }
+    }
+}
